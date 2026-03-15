39794f3db6645aaa506e964a8a2004b36ad7ad5d
Gustavo Adolfo Muñoz Reyes <tavoxpau@gmail.com>
2026-03-14 20:23:10 -0500

    feat: Public Registration endopoint

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { login, register } from "./auth.controller";
-import admin from "../../middleware/admin.middleware";
 
 const router = Router();
 
-router.post("/register", admin, register);
+router.post("/register", register);
 router.post("/login", login);
 
 export default router;
